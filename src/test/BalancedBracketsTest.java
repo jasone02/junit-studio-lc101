59ca18970f76aecd7975fac5272fda24241cc37c
61524 <unconfigured@null.spigotmc.org>
2022-01-24 19:20:01 -0600

    studio 5 complete

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,63 @@ public class BalancedBracketsTest {
 
     //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void oneBracketReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void emptyString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void nonBracketString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("qwerty"));
+    }
+
+    @Test
+    public void balancedBracketInteriorString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[qwerty]"));
+    }
+
+    @Test
+    public void balancedBracketMixedString() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("qw[e]rty"));
+    }
+
+    @Test
+    public void imbalancedBracketInteriorString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[qwerty]]"));
+    }
+
+    @Test
+    public void imbalancedBracketMixedString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("qw[e]rt]y"));
+    }
+
+    @Test
+    public void backwardsBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void backwardsBracketInteriorString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]qwerty["));
+    }
+
+    @Test
+    public void backwardsBracketMixedString() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("qw]e[rty"));
+    }
+
+    @Test
+    public void imbalancedBackwardsBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][["));
+    }
 
 }
